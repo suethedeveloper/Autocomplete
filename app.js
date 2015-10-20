18377bf9234ac72018b9af231fbb88f98c56c935
Sue Lucas <choi.soyoung@gmail.com>
2015-10-20 13:53:28 -0700

    Clean up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var arrList = [];
 
 function showAutocomplete(field, arr, displayResult) {
+  
   //array created by user
   if (arr.length < 1) {
     arr = arrList;
@@ -8,7 +9,6 @@ function showAutocomplete(field, arr, displayResult) {
   
   //get a text from user input
   var input = document.querySelector(field);
-  console.log('input', input);
   var inputVal = input.value;
   var result = [];
 
@@ -23,6 +23,7 @@ function showAutocomplete(field, arr, displayResult) {
   
   var div = document.getElementById(displayResult);
   div.innerHTML = "";
+  
   //if there is any match from user input then display
   for (var j=0; j < result.length; j++){
     div.innerHTML = div.innerHTML + result[j]+"<br>";
@@ -30,24 +31,25 @@ function showAutocomplete(field, arr, displayResult) {
 } 
 
 //A user's own creation of a list
-function createList(){
-  var arrItem = document.querySelector("input[name=list]").value;
+function createList(field){
+  var arrItem = document.querySelector("input[name=createList]").value;
 
   if (arrItem.length > 0) {
     arrList.push(arrItem);
   }
 
-  document.querySelector("input[name=list]").value = "";
-  var showListDiv = document.getElementById('showListDiv');
+  document.querySelector(field).value = "";
+  var showListDiv = document.getElementById('showCreatedList');
   showListDiv.innerHTML = "";
   //if there is any match from user input then display
-  for (var k=0; k < arrList.length; k++){
-    showListDiv.innerHTML = showListDiv.innerHTML + arrList[k]+"<br>";
+  for (var i=0; i < arrList.length; i++){
+    showListDiv.innerHTML = showListDiv.innerHTML + arrList[i]+"<br>";
   }   
   return arrList;
 }
 
+//show/hide div
 function showCreatedList(){
-  document.getElementById('createList').style.display = "none";
+  document.getElementById('divCreateList').style.display = "none";
   document.getElementById('divShowList').style.display = "block";
 }
