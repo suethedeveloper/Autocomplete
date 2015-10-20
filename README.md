8eaf93fe7d0a65ad645e0a693fcd3c6952ab69d2
Sue Lucas <choi.soyoung@gmail.com>
2015-10-20 11:31:51 -0700

    Make code more flexible

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Create a UI component similar to jQuery Autocomplete
 
 ### Direction
 
-Write an auto-complete component using HTML5/CSS3 without a javascript framework (jQuery, Underscore, etc.).
+	Write an auto-complete component using HTML5/CSS3 without a javascript framework (jQuery, Underscore, etc.).
 
-Given the array of possible values such as [‘apple’, ‘orange’, ‘banana’], typing ‘ap’ into the auto-complete component should show ‘apple’ as a possible value. Typing ‘an’ should show ‘orange’ and ‘banana’ as options.
+	Given the array of possible values such as [‘apple’, ‘orange’, ‘banana’], typing ‘ap’ into the auto-complete component should show ‘apple’ as a possible value. Typing ‘an’ should show ‘orange’ and ‘banana’ as options.
+
+### Note
+  1. Basic code with hard-coded array.
+    When keyup event occurs, display autocomplete from pre-entered list by calling showAutocomplete function
+  2. Create user's own list
+    - Create list array when a user enter an item and click save. This will trigger createList function 
+    - When a user click a  done button, hide create list div and display search input field. 
+    - When keyup event occurs, display autocomplete from an array that a user just created by calling showAutocomplete function
+
+    
--- app.js
+++ app.js
@@ -1,20 +1,53 @@
-function showAutocomplete() {
-  var input = document.querySelector("input[name=name]");
-  var inputVal = input.value;
-  var fruits = ["Apple", "Banana", "Orange"];
-  var result = [];
-  if (inputVal.length > 0) {
-    for (var i=0; i < fruits.length; i++){
-      if (fruits[i].toLowerCase().includes(inputVal.toLowerCase())){
-        result.push(fruits[i]);
-      }
-    }
-    // console.log(result);
+var arrList = [];
+
+function showAutocomplete(field, arr, displayResult) {
+  //array created by user
+  if (arr.length < 1) {
+    arr = arrList;
   }
   
-  var div = document.getElementById('result');
+  //get a text from user input
+  var input = document.querySelector(field);
+  console.log('input', input);
+  var inputVal = input.value;
+  var result = [];
+
+  if (inputVal.length > 0) {
+    //Loop through arr arrary and see if there is any match of word that contains a string from user input
+    for (var i=0; i < arr.length; i++){
+      if (arr[i].toLowerCase().includes(inputVal.toLowerCase())){
+        result.push(arr[i]);
+      }
+    }
+  }
+  
+  var div = document.getElementById(displayResult);
   div.innerHTML = "";
+  //if there is any match from user input then display
   for (var j=0; j < result.length; j++){
     div.innerHTML = div.innerHTML + result[j]+"<br>";
   }  
 } 
+
+//A user's own creation of a list
+function createList(){
+  var arrItem = document.querySelector("input[name=list]").value;
+
+  if (arrItem.length > 0) {
+    arrList.push(arrItem);
+  }
+
+  document.querySelector("input[name=list]").value = "";
+  var showListDiv = document.getElementById('showListDiv');
+  showListDiv.innerHTML = "";
+  //if there is any match from user input then display
+  for (var k=0; k < arrList.length; k++){
+    showListDiv.innerHTML = showListDiv.innerHTML + arrList[k]+"<br>";
+  }   
+  return arrList;
+}
+
+function showCreatedList(){
+  document.getElementById('createList').style.display = "none";
+  document.getElementById('divShowList').style.display = "block";
+}
